9148eef080c1727ae05329a9eff840389affb6f0
Kevin Tokheim & jordan navratil <student@epicodus.com>
2015-09-15 11:44:31 -0700

    add an option to add a business address.

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,43 @@ function Contact(firstName, lastName) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.addresses = [];
+  this.businessAddresses = [];
 }
 
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
+}
+
+function Business(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+Business.prototype.fullBusinessAddress = function(){
+  return this.street + ", " + this.city + ", " + this.state;
+}
+
+function resetFields() {
+  $("input#new-first-name").val("");
+  $("input#new-last-name").val("");
+  $("input.new-street").val("");
+  $("input.new-city").val("");
+  $("input.new-state").val("");
+  $("input.new-business-street").val("");
+  $("input.new-business-city").val("");
+  $("input.new-business-state").val("");
+}
 
 $(document).ready(function() {
   $("#add-address").click(function() {
@@ -32,17 +67,27 @@ $(document).ready(function() {
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
+
     $(".new-address").each(function() {
       var inputtedStreet = $(this).find("input.new-street").val();
       var inputtedCity = $(this).find("input.new-city").val();
       var inputtedState = $(this).find("input.new-state").val();
 
-      var newAddress = { street: inputtedStreet, city: inputtedCity, state: inputtedState };
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
       newContact.addresses.push(newAddress);
     });
 
+    $(".new-business-address").each(function() {
+      var inputtedStreet = $(this).find("input.new-business-street").val();
+      var inputtedCity = $(this).find("input.new-business-city").val();
+      var inputtedState = $(this).find("input.new-business-state").val();
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+      var newBusinessAddress = new Business(inputtedStreet, inputtedCity, inputtedState);
+      newContact.businessAddresses.push(newBusinessAddress);
+    });
+
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $(".contact").last().click(function() {
       $("#show-contact").show();
@@ -55,12 +100,12 @@ $(document).ready(function() {
       newContact.addresses.forEach(function(address) {
         $("ul#addresses").append("<li>" + address.street + ", " + address.city + ", " + address.state + "</li>");
       });
-    });
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input.new-street").val("");
-    $("input.new-city").val("");
-    $("input.new-state").val("");
+      $("ul#businessAddresses").text("");
+      newContact.businessAddresses.forEach(function(businessAddress) {
+        $("ul#businessAddresses").append("<li>" + businessAddress.street + ", " + businessAddress.city + ", " + businessAddress.state + "</li>");
+      });
+    });
+    resetFields();
   });
 });
--- specs/specs.js
+++ specs/specs.js
@@ -4,5 +4,39 @@ describe('Contact', function() {
     expect(testContact.firstName).to.equal("Rita");
     expect(testContact.lastName).to.equal("Moreno");
     expect(testContact.addresses).to.eql([]);
+    expect(testContact.businessAddresses).to.eql([]);
+  });
+
+  it("adds the fullName method to all contacts", function() {
+    var testContact = new Contact("Sherlock", "Holmes");
+    expect(testContact.fullName()).to.equal("Sherlock Holmes");
+  })
+});
+
+describe('Address', function() {
+  it("creates a new address with the given specifications", function() {
+    var testAddress = new Address("123 Main St", "Test City", "Test State");
+    expect(testAddress.street).to.equal("123 Main St");
+    expect(testAddress.city).to.equal("Test City");
+    expect(testAddress.state).to.equal("Test State");
+  });
+
+  it("adds the fullAddress method to all addresses", function() {
+    var testAddress = new Address("123 Main St", "Test City", "Test State");
+    expect(testAddress.fullAddress()).to.equal("123 Main St, Test City, Test State");
+  });
+});
+
+describe('businessAddress', function() {
+  it("creates a new business address with the given specifications", function(){
+    var testBusinessAddress = new businessAddress("400 SW 6th Ave", "Portland", "Oregon");
+    expect(testBusinessAddress.street).to.equal("400 SW 6th Ave");
+    expect(testBusinessAddress.city).to.equal("Portland");
+    expect(testBusinessAddress.state).to.equal("Oregon");
+  });
+
+  it("adds the fullBusinessAddress method to all addresses", function() {
+    var testBusinessAddress = new businessAddress("400 SW 6th Ave", "Portland", "Oregon");
+    expect(testBusinessAddress.fullBusinessAddress()).to.equal("400 SW 6th Ave, Portland, Oregon");
   });
 });
